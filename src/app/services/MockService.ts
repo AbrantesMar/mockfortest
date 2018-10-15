e08dbb8f75b09c23e6d497da1bbf094e9eb93f2c
MaryCaroline <carolinep_16@hotmail.com>
2018-10-15 10:33:04 -0300

    services

--- src/app/services/MockService.ts
+++ src/app/services/MockService.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment';
 // Models
 import { MotorAdt } from '../models/motor-adt.model';
 
@@ -14,14 +15,14 @@ export class MockService {
   ) { }
 
   getMotorAdts(): Observable<MotorAdt[]>  {
-    return this.http.get<MotorAdt[]>('api');
+    return this.http.get<MotorAdt[]>(`${environment.api}/mock`);
   }
 
   postMotorAdts(motorAdt: MotorAdt)  {
-    return this.http.post<any>('api', motorAdt);
+    return this.http.post<any>(`${environment.api}/mock`, motorAdt);
   }
 
   deleteMotorAdts(idMotorAdt: number)  {
-    return this.http.delete<any>(`api/${idMotorAdt}`);
+    return this.http.delete<any>(`${environment.api}/mock/${idMotorAdt}`);
   }
 }
